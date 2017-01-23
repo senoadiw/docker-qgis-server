8aede4fd4379e7dc61c96b81fed62760bbb3f4f1
Seno @ ThinkPad <senoadiw@gmail.com>
2017-01-23 18:08:00 +0700

    update: new qgis-server 2.14 build

--- 2.14/build/getDeps.sh
+++ 2.14/build/getDeps.sh
@@ -9,3 +9,7 @@ LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get update &&  \
         xfonts-base xfonts-scalable xvfb \
         apache2 libapache2-mod-fcgid git
 
+mkdir -p /opt/qgis-server/plugins
+cd /opt/qgis-server/plugins
+git clone https://github.com/kartoza/otf-project.git
+cd otf-project/ && git checkout 0a44fa4ccc40a5645af7d4349c63b9bb2f995595
